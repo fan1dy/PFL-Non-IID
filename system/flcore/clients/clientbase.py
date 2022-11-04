1e63c963beff522668bb6b66fcf5e9dde01edd18
Dongyang Fan <fan1dy97@gmail.com>
2022-11-04 13:56:10 +0000

    small modifications

--- system/flcore/clients/clientbase.py
+++ system/flcore/clients/clientbase.py
@@ -15,7 +15,7 @@ class Client(object):
     Base class for clients in federated learning.
     """
 
-    def __init__(self, args, id, train_samples, test_samples, **kwargs):
+    def __init__(self, args, id, train_samples, test_samples,ref_samples, **kwargs):
         self.model = copy.deepcopy(args.model)
         self.dataset = args.dataset
         self.device = args.device
@@ -25,6 +25,7 @@ class Client(object):
         self.num_classes = args.num_classes
         self.train_samples = train_samples
         self.test_samples = test_samples
+        self.ref_samples = ref_samples
         self.batch_size = args.batch_size
         self.learning_rate = args.local_learning_rate
         self.local_steps = args.local_steps
@@ -55,8 +56,14 @@ class Client(object):
     def load_test_data(self, batch_size=None):
         if batch_size == None:
             batch_size = self.batch_size
-        test_data = read_client_data(self.dataset, self.id, is_train=False)
+        test_data, _ = read_client_data(self.dataset, self.id, is_train=False)
         return DataLoader(test_data, batch_size, drop_last=False, shuffle=True)
+
+    def load_ref_data(self, batch_size=None):
+        if batch_size == None:
+            batch_size = self.batch_size
+        _, ref_data = read_client_data(self.dataset, self.id, is_train=False)
+        return DataLoader(ref_data, batch_size, drop_last=False, shuffle=True)
         
     def set_parameters(self, model):
         for new_param, old_param in zip(model.parameters(), self.model.parameters()):
@@ -129,6 +136,41 @@ class Client(object):
         # self.save_model(self.model, 'model')
 
         return loss, train_num
+
+    def ref_metrics(self):
+        refloaderfull = self.load_ref_data()
+        # self.model = self.load_model('model')
+        # self.model.to(self.device)
+        self.model.eval()
+
+        ref_acc = 0
+        ref_num = 0
+        y_prob = []
+        y_true = []
+        
+        with torch.no_grad():
+            for x, y in refloaderfull:
+                if type(x) == type([]):
+                    x[0] = x[0].to(self.device)
+                else:
+                    x = x.to(self.device)
+                y = y.to(self.device)
+                output = self.model(x)
+
+                ref_acc += (torch.sum(torch.argmax(output, dim=1) == y)).item()
+                ref_num += y.shape[0]
+
+                y_prob.append(F.softmax(output).detach().cpu().numpy())
+                y_true.append(label_binarize(y.detach().cpu().numpy(), classes=np.arange(self.num_classes)))
+
+        # self.model.cpu()
+
+        y_prob = np.concatenate(y_prob, axis=0)
+        y_true = np.concatenate(y_true, axis=0)
+
+        auc = metrics.roc_auc_score(y_true, y_prob, average='micro')
+        
+        return ref_acc, ref_num, auc
 
     # def get_next_train_batch(self):
     #     try:
